8effb389ce50379b06a9c722917641b4a2ef6b1d
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2020-12-31 14:25:03 +0100

    feat: export exit code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 extern crate libc;
 
 use libc::c_char;
+use libc::c_int;
 use std::ffi::CStr;
 use std::ffi::CString;
 use std::process::Command;
+use std::process::ExitStatus;
 use std::ptr;
 
 #[derive(Debug, Clone)]
@@ -11,15 +13,17 @@ use std::ptr;
 pub struct Response {
     pub output: *const c_char,
     pub err: *const c_char,
+    pub status_code: c_int,
 }
 
 impl Response {
-    pub fn boxed<T: Into<Vec<u8>>>(output: T, err: T) -> *mut Response {
+    pub fn boxed<T: Into<Vec<u8>>>(output: T, err: T, status_code: i32) -> *mut Response {
         let output_ptr = string_to_c_char(output);
         let err_ptr = string_to_c_char(err);
         let resp = Response {
             output: output_ptr,
             err: err_ptr,
+            status_code: status_code as c_int,
         };
         return Box::into_raw(Box::new(resp));
     }
@@ -40,6 +44,13 @@ fn string_to_c_char<T: Into<Vec<u8>>>(value: T) -> *const c_char {
     return value_ptr;
 }
 
+fn get_safe_status_code(status: ExitStatus) -> i32 {
+    match status.code() {
+        Some(code) => return code,
+        None       => return 0
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn getCommandOutput(command: *const c_char, args: *const c_char) -> *mut Response {
     let str_command = c_char_to_string(command);
@@ -52,10 +63,11 @@ pub extern "C" fn getCommandOutput(command: *const c_char, args: *const c_char) 
     let output = cmd.output();
     match output {
         Ok(cmd) => {
-            return Response::boxed(cmd.stdout, cmd.stderr);
+            let status_code = get_safe_status_code(cmd.status);
+            return Response::boxed(cmd.stdout, cmd.stderr, status_code);
         }
         Err(_e) => {
-            return Response::boxed("", "Command::new failed");
+            return Response::boxed("", "Command::new failed", 0);
         }
     }
 }
--- test/main.go
+++ test/main.go
@@ -10,13 +10,20 @@ import "C"
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"io"
-	"log"
 	"os/exec"
 	"strings"
 	"unsafe"
 )
+
+type commandError struct {
+	err      string
+	exitCode int
+}
+
+func (e *commandError) Error() string {
+	return e.err
+}
 
 type cmd struct{}
 
@@ -42,15 +49,24 @@ func (c *cmd) runCommand(command string, args ...string) (string, error) {
 	cmd := exec.Command(command, args...)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Fatal(err)
+		return "", &commandError{
+			err:      err.Error(),
+			exitCode: 666,
+		}
 	}
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		log.Fatal(err)
+		return "", &commandError{
+			err:      err.Error(),
+			exitCode: 667,
+		}
 	}
 	err = cmd.Start()
 	if err != nil {
-		return "", err
+		return "", &commandError{
+			err:      err.Error(),
+			exitCode: 668,
+		}
 	}
 	defer func() {
 		_ = cmd.Process.Kill()
@@ -58,7 +74,13 @@ func (c *cmd) runCommand(command string, args ...string) (string, error) {
 	stdoutString := getOutputString(stdout)
 	stderrString := getOutputString(stderr)
 	if stderrString != "" {
-		return "", errors.New(stderrString)
+		// only wait in case of error reduces the lead time on successful
+		// commands on windows due to not calling process.Wait()
+		_ = cmd.Wait()
+		return "", &commandError{
+			err:      stderrString,
+			exitCode: cmd.ProcessState.ExitCode(),
+		}
 	}
 	return stdoutString, nil
 }
@@ -80,7 +102,10 @@ func (c *cmd) runCommandFromRust(command string, args ...string) (string, error)
 	output := C.GoString(response.output)
 	err := C.GoString(response.err)
 	if err != "" {
-		return "", errors.New(cleanOutput(err))
+		return "", &commandError{
+			err:      cleanOutput(err),
+			exitCode: int(response.status_code),
+		}
 	}
 	return cleanOutput(output), nil
 }
